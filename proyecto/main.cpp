d9f94193b6e7249081363fdf1c7f6a6dd4227d27
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-08-22 09:48:11 -0500

    practica1

---
+++ proyecto/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*float a;
+    int b;
+    int z;
+    int x;
+    float y;
+    cout << " a: ";
+    cin >> a;
+    cout << " b: ";
+    cin >> b;
+    z=a+b;
+    x=a*b;
+    y=a/b;
+    cout << " la suma de " << a << " y " << b << " es " << z << endl;
+    cout << " el producto de " << a << " y " << b << " es " << x << endl;
+    cout << " la division de " << a << " y " << b << " es " << y << endl;
+
+    //cout << z << endl;
+    return 0;
+    */
+    int a;
+    int z;
+    a = 32766;
+    z = a + 1;
+    cout << z << endl;
+    z = z + 1;
+    cout << z << endl;
+    return 0;
+}
